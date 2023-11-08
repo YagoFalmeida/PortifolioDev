d54339bbd3afd3486a7968568b781b0eeae820b8
Yago Almeida <yago.almeida@patmos.net.br>
2023-11-08 11:03:26 -0300

    Remoção de todos os arquivos e pastas presentes no reposítório.

--- PortifolioDevWebSite/PortifolioDevWebSite/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PortifolioDevWebSite.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
