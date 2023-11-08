c8ad41615a01e14b36cf310ab490b51d849d0ed4
Yago Almeida <yago.almeida@patmos.net.br>
2023-11-08 10:36:03 -0300

    - Commit inicial.

---
+++ PortifolioDevWebSite/PortifolioDevWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortifolioDevWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
