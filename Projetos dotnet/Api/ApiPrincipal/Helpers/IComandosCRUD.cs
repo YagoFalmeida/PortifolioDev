d5a090493598f9c0b19e8dc834871d0ec73c15f6
yago.almeida <yago.almeida@patmos.net.br>
2024-09-16 17:52:56 -0300

    - Adição de novo projeto.

---
+++ Projetos dotnet/Api/ApiPrincipal/Helpers/IComandosCRUD.cs
@@ -0,0 +1,11 @@
+﻿namespace Helpers
+{
+    public interface IComandosCRUD<T>
+    {
+        public T Atualizar(T obj);
+        public T Cadastrar(T obj);
+        public T Encontrar(long identificacao);
+        public List<T> Listar();
+        public Boolean Deletar();
+    }
+}
